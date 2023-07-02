cc37cfd5aaf2e66e7b90c0439a0b3a36bc04d306
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-07-02 19:38:53 +0300

    -Implementation of new Registration page

--- ToDoMicro-ui/src/app/app-routing.module.ts
+++ ToDoMicro-ui/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './authentication/registration/registration.component';
+import { LoginuserComponent } from './authentication/loginuser/loginuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginuserComponent
+  },
+  {
+    path:'register',
+    component:RegistrationComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
